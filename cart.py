7793224e73ebde640c96dab0a8ce23c4e4ff8b90
Febria Roosita Dwi <froositadwi@cermati-dev>
2018-02-21 00:25:46 +0700

    change to class

--- cart.py
+++ cart.py
@@ -1,23 +1,25 @@
-product = {}
+class Cart:
+	def __init__(self):
+		self.product = {}
+	def addProduct(self, productCode, quantity):
+		if productCode in self.product:
+			self.product[productCode] = self.product[productCode] + quantity
+		else:
+			self.product[productCode] = quantity
 
-def addProduct(productCode, quantity):
-	if productCode in product:
-		product[productCode] = product[productCode] + quantity
-	else:
-		product[productCode] = quantity
+	def removeProduct(self, productCode):
+		if productCode in self.product:
+			del self.product[productCode]
 
-def removeProduct(productCode):
-	if productCode in product:
-		del product[productCode]
+	def showCart(self):
+		for cart in self.product:
+			print "{} ({})".format(cart, self.product[cart])
 
-def showCart():
-	for cart in product:
-		print "{} ({})".format(cart, product[cart])
-
-addProduct("Baju Merah Mantap", 1)
-addProduct("Baju Merah Mantap", 1)
-addProduct("Bukuku", 3)
-removeProduct("Bukuku")
-addProduct("Singlet Hijau", 1)
-removeProduct("ProdukBohongan")
-showCart()
+cart = Cart()
+cart.addProduct("Baju Merah Mantap", 1)
+cart.addProduct("Baju Merah Mantap", 1)
+cart.addProduct("Bukuku", 3)
+cart.removeProduct("Bukuku")
+cart.addProduct("Singlet Hijau", 1)
+cart.removeProduct("ProdukBohongan")
+cart.showCart()
